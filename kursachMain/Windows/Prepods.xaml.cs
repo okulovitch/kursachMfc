4bb424f53cc52b63d00abbb88bbb76ae2d00314a
Okulovitch Vladimir <okulovitch.vladmir@yandex.ru>
2017-04-09 23:07:56 +0400

    test connect prepods to database

--- kursachMain/Windows/Prepods.xaml.cs
+++ kursachMain/Windows/Prepods.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Navigation;
+
 
 namespace kursachMain.Windows
 {
@@ -20,17 +23,74 @@ namespace kursachMain.Windows
     /// </summary>
     public partial class Prepods : Window
     {
+        
+
         public Prepods()
         {
             InitializeComponent();
+            //string connectionString = "SERVER=localhost;DATABASE=kursach;UID=root;PASSWORD=";
+            //MySqlConnection = new MySq
         }
 
         private void btn_Save_Click(object sender, RoutedEventArgs e)///save in database Prepods 
         {
+            string serverName = "localhost"; // Адрес сервера (для локальной базы пишите "localhost")
+            string userName = "admin"; // Имя пользователя
+            string dbName = "kursach"; //Имя базы данных
+            string port = "3306"; // Порт для подключения
+            string password = ""; // Пароль для подключения
+            string dbConnectionString = "server=" + serverName +
+                ";user=" + userName +
+                ";database=" + dbName +
+                ";port=" + port +
+                ";password=" + password + ";";      
+            SqlConnection sqlCon = new SqlConnection(dbConnectionString);
+            //open connection database 
+            try
+            {
+                sqlCon.Open();
+                string Query = "insert into Преподаватели (IDПреподавателя,ФИОПреподавателя,Должность,Кафедра)  values ('"+ this.FIO_txt.Text + "','"+ this.Positin_txt.Text+ "','"+ Pulpit_txt.Text +"')";
+                SqlCommand createCommand = new SqlCommand(Query, sqlCon);
+                createCommand.ExecuteNonQuery();
+                MessageBox.Show("saved");
 
+                sqlCon.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         
         private void Button_Click(object sender, RoutedEventArgs e)///show sql database Prepods
+        {
+            SqlConnection sqlCon = new SqlConnection();
+            //open connection database 
+            try
+            {
+                sqlCon.Open();
+                string Query = "select *from Преподаватели";
+                SqlCommand createCommand = new SqlCommand(Query, sqlCon);
+                createCommand.ExecuteNonQuery();
+
+                sqlCon.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
 
         }
